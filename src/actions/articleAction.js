c6931b4b4ca99a93cc45228095d055c32f2d71f6
floatinfeel <dandythamrin@gmail.com>
2021-02-15 14:33:21 +0700

    change server

--- src/actions/articleAction.js
+++ src/actions/articleAction.js
@@ -3,7 +3,7 @@ import {SENDING_REQUEST, REQUEST_FINISHED, SET_POSTS} from '../constants/actionT
 export const getPosts = async (dispatch) =>{
     
     try {
-        const res = await fetch('http://localhost:5000/posts')
+        const res = await fetch('https://my-json-server.typicode.com/floatinfeel/dbjson/posts')
         const data = await res.json()
         dispatch({type: SENDING_REQUEST})
         dispatch({type: REQUEST_FINISHED})
